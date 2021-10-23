0e006d1c749d620489bb7507eab263dd59e0fb07
romalynfloranda <romalynfloranda0203@gmail.com>
2021-10-24 02:18:33 +0800

    name.age.address

--- program1.py
+++ program1.py
@@ -1,11 +1,11 @@
 #variables
-namE = input("Your name:")
-agE = input("Your age:")
-addresS = input ("Your address:")
+name = input("Your name:")
+age = input("Your age:")
+address = input ("Your address:")
 
 #program
-print("Hi, my name is" ,f"{namE}.")
-print("I am" ,f"{agE} years old and I live in",f"{addresS}.")
+print("Hi, my name is" ,f"{name}.")
+print("I am" ,f"{age} years old and I live in",f"{address}.")
 
 #result should be
 #Hi, my name is _____.
