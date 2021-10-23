1d8bb8a9b9b1411603e07f2debc73c345fe74046
romalynfloranda <romalynfloranda0203@gmail.com>
2021-10-24 01:52:26 +0800

    apple.price.change

--- program3.py
+++ program3.py
@@ -1,9 +1,9 @@
 #variables
 moneY = float(input("Amount of money you have:"))
-applePrice = float (input("Price of apple per piece:"))
-maxNumberApples = int(moneY / applePrice)
-ttlPayment = maxNumberApples * applePrice
-changE =  moneY - ttlPayment 
+applePrice = float (input("Price of apple per piece:"))     #apple price per piece
+maxNumberApples = int(moneY / applePrice)                   #maximum number of apples
+ttlPayment = maxNumberApples * applePrice                   #total payment for apples
+changE =  moneY - ttlPayment                                #change
 
 #program
 print("You can buy", f"{maxNumberApples}" , "and your change is", f"{changE:.2f} pesos")
