f9dfac0d354385cbcc2ecdeff46d28d787d72f72
romalynfloranda <romalynfloranda0203@gmail.com>
2021-10-23 23:50:22 +0800

    name.age.address

--- program1.py
+++ program1.py
@@ -9,4 +9,4 @@ print("I am" ,f"{agE} years old and I live in",f"{addresS}.")
 
 #result should be
 #Hi, my name is Romalyn Bublo Floranda.
-#I am 18 years old and I live in 144-B Howmart st. Baesa Quezon City
+#I am 18 years old and I live in 144-B Howmart st. Baesa Quezon City 
