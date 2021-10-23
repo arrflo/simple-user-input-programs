f76ad52e855fa55349d9dd30a57c5adda1b9620f
romalynfloranda <romalynfloranda0203@gmail.com>
2021-10-23 22:55:19 +0800

    name.age.address

---
+++ program1.py
@@ -0,0 +1,12 @@
+#variables
+namE = input()
+agE = input()
+addresS = input ()
+
+#program
+print("Hi, my name is" ,f"{namE}.")
+print("I am" ,f"{agE} years old and I live in",f"{addresS}.")
+
+#result should be
+#Hi, my name is Romalyn Bublo Floranda.
+#I am 18 years old and I live in 144-B Howmart st. Baesa Quezon City
