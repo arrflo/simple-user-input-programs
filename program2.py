c0e2b0c4b96c0842e8a1bde3a39f961721408426
romalynfloranda <romalynfloranda0203@gmail.com>
2021-10-24 01:51:58 +0800

    apple.orange.prices

--- program2.py
+++ program2.py
@@ -1,9 +1,9 @@
 # variables
-appleS = int(input("How many apples do you want to buy?"))
-orangeS = int(input("How many oranges do you want to buy?"))
-ttlApple = appleS * 20
-ttlOrange = orangeS * 25
-ttlPrice = ttlApple + ttlOrange
+appleS = int(input("How many apples do you want to buy?"))      #number of apples
+orangeS = int(input("How many oranges do you want to buy?"))    #number of oranges
+ttlApple = appleS * 20                                          #total price of apples
+ttlOrange = orangeS * 25                                        #total price of oranges
+ttlPrice = ttlApple + ttlOrange                                 #grandtotal
 
 #program
 print("The total amount is",f"{ttlPrice} pesos.")
