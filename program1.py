8ede01e70671960bcfa0bfa68fa05335c4c94980
romalynfloranda <romalynfloranda0203@gmail.com>
2021-10-24 15:57:00 +0800

    name.age.address

--- program1.py
+++ program1.py
@@ -7,5 +7,4 @@ address = input ("Your address:")
 print("Hi, my name is" ,f"{name}.", "I am" ,f"{age} years old and I live in",f"{address}.")
 
 #result should be
-#Hi, my name is _____.
-#I am __ years old and I live in __________________ .
+#Hi, my name is _____. I am __ years old and I live in __________________ .
